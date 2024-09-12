7cdbc26f8eab77e91b94b64e5d9193b1874e7b97
Stevão Rodrigues <162615972+Stevaoo@users.noreply.github.com>
2024-09-12 20:07:49 -0300

    Update ORM.ts

--- API_Partiu/ORM.ts
+++ API_Partiu/ORM.ts
@@ -17,6 +17,39 @@ export class Usuario {
 }
 
 export class Atividades {
-    id_atividade: number;
-    nome: string;
+    private id_atividade: number;
+    public tipo_atividade: string;
+    public descricao: string;
+    public endereco: string;
+    public preco: number;
+    public data_atividade: string;
+
+    constructor(id_atividade: number, tipo_atividade: string, descricao: string, endereco: string, preco: number, data_atiividade: string) {
+        this.id_atividade = id_atividade;
+        this.tipo_atividade = tipo_atividade;
+        this.descricao = descricao;
+        this.endereco = endereco;
+        this.preco = preco;
+        this.data_atividade = data_atiividade;
+    }
+}
+
+export class Restaurante {
+    private id_restaurante: number;
+    public nome: string;
+    public culinaria: string;
+    public endereco: string;
+    public preco_faixa: number;
+    public avaliacao: number;
+    public telefone: string;
+
+    constructor(id_restaurante: number, nome: string, culinaria: string, endereco: string, preco_faixa: number, avaliacao: number, telefone: string){
+        this.id_restaurante = id_restaurante;
+        this.nome = nome;
+        this.culinaria = culinaria;
+        this.endereco = endereco;
+        this.preco_faixa = preco_faixa;
+        this.avaliacao = avaliacao;
+        this.telefone = telefone;
+    }
 }
