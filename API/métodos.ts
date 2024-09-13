2645127d89346b7874c7167fba378dfb01128f87
Thesantosz <162642046+Thesantosz@users.noreply.github.com>
2024-09-12 21:32:09 -0300

    Add files via upload

---
+++ API/métodos.ts
@@ -0,0 +1,126 @@
+import { question } from "readline-sync";
+
+let ask  = require('readline-sync')
+
+let armazenamento: any = []
+
+export class Usuario {
+    private id_usuario: number;
+    private nome: string;
+    public email: string;
+    private senha: number;
+    public data_nasc: string;
+    public data_hora: string;
+
+    constructor(id_usuario: number, nome: string, email: string, senha: number, data_nasc: string, data_hora: string){
+        this.id_usuario = id_usuario;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.data_nasc = data_nasc;
+        this.data_hora = data_hora;
+    }
+    entrarConta(): void {};
+
+    criarConta(): void {
+  
+        let perguntaNome: string = question('Nome: ')
+        let perguntaEmail = question('Email: ')
+        let perguntaSenha = question('Senha: ')
+        let perguntaConfirmaSenha = question('Confirmar senha: ')
+        let perguntaNasc = question('Data de Nascimento: ')
+
+       
+        if(perguntaSenha === perguntaConfirmaSenha || perguntaNasc >= 18){
+
+            armazenamento.push(perguntaNome, perguntaEmail, perguntaSenha)
+
+        }
+
+
+
+
+    }
+
+    removerConta(): void {}
+
+    redefinirSenha(): void {};
+
+
+
+    }
+
+
+export class Atividades {
+    private id_atividade: number;
+    public tipo_atividade: string;
+    public descricao: string;
+    public endereco: string;
+    public preco: number;
+    public data_atividade: string;
+
+    constructor(id_atividade: number, tipo_atividade: string, descricao: string, endereco: string, preco: number, data_atiividade: string) {
+        this.id_atividade = id_atividade;
+        this.tipo_atividade = tipo_atividade;
+        this.descricao = descricao;
+        this.endereco = endereco;
+        this.preco = preco;
+        this.data_atividade = data_atiividade;
+    }
+
+    vizualizarAtividades(): void {};
+
+    buscarAtividades(): void {};
+
+
+}
+
+export class Hoteis {
+    private id_hotel: number;
+    public nome: string;
+    public endereco: string;
+    public avaliacao: string;
+    public preco: number;
+    public data_hora:string;
+
+    constructor(id_hotel: number, nome: string, endereco: string, avaliacao: string, preco: number, data_hora: string){
+        this.id_hotel = id_hotel;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.avaliacao = avaliacao;
+        this.preco = preco;
+        this.data_hora = data_hora;
+    }
+
+    vizualizarHotel(): void{}
+
+    buscarHotel(): void{}
+
+    reservarQuarto():void{}
+
+
+}
+
+export class Avaliacao {
+    private id_avaliacao: number;
+    private usuario: Usuario;
+    public tipo_avaliacao: string;
+    public avaliacao: string;
+    public comentarios: string;
+    public data_hora: number;
+
+    constructor(id_avaliacao: number, usuario: Usuario, tipo_avaliacao: string, avaliacao: string, comentarios: string, data_hora: number){
+        this.id_avaliacao = id_avaliacao;
+        this.usuario = usuario;
+        this.tipo_avaliacao = tipo_avaliacao;
+        this.avaliacao = avaliacao;
+        this.comentarios = comentarios;
+        this.data_hora = data_hora;
+    }
+
+    vizualizarAvaliacoes(): void{}
+
+    avaliar(): void{}
+
+}
+
